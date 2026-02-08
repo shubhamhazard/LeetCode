4fc057938fa417d58c8fb6c4f5ee8e11e78413ba
shubhamhazard <157235877+shubhamhazard@users.noreply.github.com>
2026-02-08 19:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (5.41%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -11,9 +11,28 @@ public:
 
         return dp[n] = max(choice1,choice2);
     }
+
+    int tabulation(int n , vector<int> &nums)
+    {
+        vector<int> dp(n,0);
+        dp[0] = nums[0];
+
+        for(int i=1; i<n; i++)
+        {
+            int choice1 = nums[i];
+            if(i>1)
+                choice1 += dp[i-2];
+            
+            int choice2 = dp[i-1];
+
+            dp[i] = max(choice1, choice2);
+        }
+        return dp[n-1];
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+1, -1);
-        return maxAmount(n-1,nums,dp);
+        // return maxAmount(n-1,nums,dp);
+        return tabulation(n,nums);
     }
 };
