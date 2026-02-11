b308af6d888bc0eb84c394a120cd94f697c85072
shubhamhazard <157235877+shubhamhazard@users.noreply.github.com>
2026-02-11 16:41:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (43.85%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -16,9 +16,32 @@ public:
 
         return dp[idx] = result;
     }
+
+    int bottomUp(string s)
+    {
+        int n = s.length();
+        vector<int> t(n+1, 0);
+
+        t[n] = 1;
+        for(int i=n-1; i>=0; i--)
+        {
+            if(s[i] == '0')
+                t[i] = 0;
+            else{
+                t[i] += t[i+1];
+
+                if(i+1 < n){
+                    if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))
+                        t[i] += t[i+2];
+                }
+            }
+        }
+        return t[0];
+    }
     int numDecodings(string s) {
         int n = s.length();
         vector<int> dp(n+1, -1);
-        return solve(0, s, n,dp);
+        // return solve(0, s, n,dp);
+        return bottomUp(s);
     }
 };
